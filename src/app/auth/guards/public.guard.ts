22248800d88c7fdbf499b0cd4caeea16dd97852b
Ivan Panussis C <ivan.panussis.ing@gmail.com>
2023-09-16 04:15:55 +0100

    Heroes app Angular

---
+++ src/app/auth/guards/public.guard.ts
@@ -0,0 +1,40 @@
+import { inject } from '@angular/core';
+import { Observable, tap, map } from 'rxjs';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  CanMatchFn,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+const checkAuthStatus = (): boolean | Observable<boolean> => {
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  return authService.checkAuthentication().pipe(
+    tap((isAuthenticated) => {
+      if (isAuthenticated) {
+        router.navigate(['/heroes/list']);
+      }
+    }),
+    map( isAuthenticated => !isAuthenticated )
+  );
+};
+
+export const canMatchGuard: CanMatchFn = (
+  route: Route,
+  segments: UrlSegment[]
+  ) => {
+    return checkAuthStatus();
+  };
+
+export const canActivateGuard: CanActivateFn = (
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ) => {
+    return checkAuthStatus();
+};
